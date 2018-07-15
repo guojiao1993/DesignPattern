f0e4196f06db7181d282f4dd9ecd037a8539301c
guojiao1993 <wang2012jida@163.com>
2018-07-16 00:00:07 +0800

    增加简单工厂(sf)、工厂方法(f)和抽象工厂(af)模式

---
+++ src/factory/pizzaf/NYPizzaStore.java
@@ -0,0 +1,19 @@
+package factory.pizzaf;
+
+public class NYPizzaStore extends PizzaStore {
+
+	@Override
+	Pizza createPizza(String item) {
+		if ("cheese".equals(item)) {
+			return new NYStyleCheesePizza();
+		} else if ("veggie".equals(item)) {
+			return new NYStyleVeggiePizza();
+		} else if ("clam".equals(item)) {
+			return new NYStyleClamPizza();
+		} else if ("pepperoni".equals(item)) {
+			return new NYStylePepperoniPizza();
+		} else {
+			return null;
+		}
+	}
+}
